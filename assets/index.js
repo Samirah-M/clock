e4745299be3224b03858e9ed063f39995f1bd9da
Samirah-M <143946932+Samirah-M@users.noreply.github.com>
2024-01-29 23:12:51 +0000

    current day and time code added, hours and mins calculations added

---
+++ assets/index.js
@@ -0,0 +1,30 @@
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+    // get current date and time
+    const now = new Date();
+   
+    // get and calculate seconds hand degree
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+   
+    // get and calculate minutes hand degree
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+   
+    // get and calculate hours hand degree
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+   }
+
+// update time every 1000ms (1 second)
+setInterval(setDate, 1000);
+
+// initialize clock hands to current time
+setDate();
